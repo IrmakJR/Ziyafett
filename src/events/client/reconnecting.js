95531344148086e5acaa1934f7860d70e3e93ed9
IrmakJR <106346932+IrmakJR@users.noreply.github.com>
2023-01-08 13:13:53 +0300

    Ziyafett

---
+++ src/events/client/reconnecting.js
@@ -0,0 +1,13 @@
+//here the event starts
+module.exports = client => {
+    console.log(`Reconnceting at ${new Date()}.`.bgYellow.black)
+}
+/**
+  * @INFO
+  * Bot Coded by Veloç#3169 | https://discord.gg/FQGXbypRf8
+  * @INFO
+  * Work for Milrato Development | https://milrato.eu
+  * @INFO
+  * Please mention him / Milrato Development, when using this Code!
+  * @INFO
+*/
---
+++ src/events/guild/threadCreate.js
@@ -0,0 +1,18 @@
+module.exports = async (client, thread) => {
+    if(thread.joinable){
+        try{
+            await thread.join();
+        }catch (e){
+            console.log(e)
+        }
+    }
+}
+/**
+ * @INFO
+ * Bot Coded by Veloç#3169 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
